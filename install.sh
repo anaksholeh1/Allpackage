63dc4e68087bb5a1908e6c9773d0c1d936efa84f
anaksholeh_ <73272485+anaksholeh1@users.noreply.github.com>
2021-08-01 10:06:37 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!urs/bin/bash
+
+apt-get install python
+apt-get install python2
+apt-get install nano
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+
+
+
+echo "SEMUA PACKAGE SUDAH TERINSTAL
+login
+cd $HOME
